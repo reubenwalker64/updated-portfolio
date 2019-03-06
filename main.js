efa1022b0d5b831614bbc8416242ac2d5809c660
Reuben Walker, Jr <rwalker@mobile-atom.com>
2019-03-06 15:29:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+   apiKey: "AIzaSyCj6oROILncEzNuUGaddtobixQKG1qIVOU",
+    authDomain: "form-db-mac.firebaseapp.com",
+    databaseURL: "https://form-db-mac.firebaseio.com",
+    projectId: "form-db-mac",
+    storageBucket: "form-db-mac.appspot.com",
+    messagingSenderId: "55712442460"
+};
+firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    message:message
+  });
+}
